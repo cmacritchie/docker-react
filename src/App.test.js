682f75ee779182bc2e912d6f810781ff07a5b5f7
Craig MacRitchie <craig.macritchie@edu.sait.ca>
2020-01-08 18:40:38 -0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/hi Craig/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/hi Craig/i);
   expect(linkElement).toBeInTheDocument();
 });
